da33e4b7e3e0a0776ff4cea4a16090f8b66c0a7a
dsbowen <dsbowen@wharton.upenn.edu>
2020-06-04 20:54:22 -0400

    fixed target selector bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sqlalchemy-mutablesoup",
-    version="0.0.7",
+    version="0.0.8",
     author="Dillon Bowen",
     author_email="dsbowen@wharton.upenn.edu",
     description="Mutable BeautifulSoup database type",
